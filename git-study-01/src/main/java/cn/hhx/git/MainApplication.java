79e07f7debadf8be281e9172be36f76d809f038b
hhx <1739173733@qq.com>
2023-02-10 16:04:10 +0800

    commit1

---
+++ git-study-01/src/main/java/cn/hhx/git/MainApplication.java
@@ -0,0 +1,11 @@
+package cn.hhx.git;
+
+/**
+ * @author hhxStellar
+ * @date 2023/2/10-16:03
+ */
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
